809497d539f6856c87b6dd9f29395ae1c8aa0350
Max <446964321@qq.com>
2017-10-20 18:18:39 +0800

    add basic function of home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,20 +12,51 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  List v2List;
+class _ContentItem extends StatelessWidget {
+  String _author;
+  String _content;
 
-  void _incrementCounter() {
-    Request req = new Request('https://www.v2ex.com/api/topics/latest.json' ,{}, RequestMethod.Get);
-    req.start((response) {
-      try {
-        List data = JSON.decode(response.body);
-      } catch(e) {
-        print(e);
-      }
-    });
+  _ContentItem(this._author, this._content);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: const EdgeInsets.only(top: 5.0),
+        color: Colors.red[300],
+        child: new Padding(
+          padding: new EdgeInsets.all(8.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              new Text(_author),
+              new Text(_content),
+            ]
+            )
+          )
+        );
+
   }
+}
+class _ContentView  extends StatelessWidget {
+  List<Widget> _list = <Widget>[];
+
+  _ContentView(this._list);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        color: Colors.green[300],
+        padding: const EdgeInsets.all(12.0),
+        child: new ListView(
+          children:  _list
+
+          ),
+        );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List v2List;
 
   void _queryV2List() {
     if (v2List != null) {
@@ -35,6 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
     req.start((response) {
       try {
         List data = JSON.decode(response.body);
+        print(data);
         setState(() {
           v2List = data;
         });
@@ -47,11 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
   List<Widget> _getV2Feed() {
     List<Widget> _v2List = <Widget>[];
     for (var i = 0, len = v2List.length; i < len; ++i) {
-      String t = v2List[i]['title'];
-      Text wid = new Text(t);
-      _v2List.add(wid);
+      String author = v2List[i]['member']['username'];
+      String content = v2List[i]['title'];
+      _ContentItem item = new _ContentItem(author, content);
+      _v2List.add(item);
     }
-
     return _v2List;
   }
 
@@ -64,17 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: new AppBar(
           title: new Text(widget.title),
           ),
-        body: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: _getV2Feed(),
-            ),
-          ),
-        // floatingActionButton: new FloatingActionButton(
-          // onPressed: _incrementCounter,
-          // tooltip: 'Increment',
-          // child: new Icon(Icons.add),
-          // ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: new _ContentView(_getV2Feed()),
         );
   }
 }
